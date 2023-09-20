9fea4fb8613d0a91dbc93f245637d7ef1d4de470
Astitva Jaiswal <astitvajaiswalreturns@gmail.com>
2023-09-20 18:44:39 +0530

    script.js

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-// Get references to the button and the element to change text
-const changeTextButton = document.getElementById('changeTextButton');
-const textToChange = document.getElementById('textToChange');
+// Get a reference to the element whose text you want to change
+const changingText = document.getElementById('changingText');
 
-// Define a function to change the text
-function changeText() {
-    textToChange.textContent = 'Text has been changed!';
+// Define an array of texts to cycle through
+const texts = [
+    'github',
+    'kaggle',
+    'leetcode'
+];
+
+let currentIndex = 0;
+
+// Function to change the text on an interval
+function changeTextOnInterval() {
+    changingText.textContent = texts[currentIndex];
+    currentIndex = (currentIndex + 1) % texts.length;
 }
 
-// Add a click event listener to the button
-changeTextButton.addEventListener('click', changeText);
+// Change the text immediately when the page loads
+changeTextOnInterval();
+
+// Set an interval to change the text every 2 seconds (2000 milliseconds)
+setInterval(changeTextOnInterval, 2000);
